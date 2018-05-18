1436c9cfcc54e5ca8e587dd4124807365cdf7bc7
mtotodev05 <mtotodev05@gmail.com>
2018-05-18 17:22:38 +0300

    updated functions

--- server.js
+++ server.js
@@ -6,16 +6,33 @@ const logger = require('morgan')
 const port = process.env.PORT || 3030
 
 var app = express();
-
+function processBankTransaction(){
+    app.use(logger('dev'));
+    app.use(parser.json());
+    app.use(parser.urlencoded({extended:true}));
+    app.get('*',(req,res)=>{
+        res.send('XYZ Bank USSD Module');
+    });
+    app.post('*',(req,res) =>{
+        let {account_number, account_balance,text} = req.body
+        if(text ==''){
+            let response  = `CON Welcome To XYZ Bank, Please Select
+            1. Check Account Balance
+            2. Know Your Account Number`
+            res.send(response);
+        }else if(text == '1'){
+            account_balance = 'KSH 10,000';
+            res.send(account_balance);
+        }
+    });
+}
 function  processRequest() {
     app.use(logger('dev'))
     app.use(parser.json())
     app.use(parser.urlencoded({extended:true}))
-
     app.get('*',(req,res) =>{
         res.send('USSD application ');
     });
-
     app.post('*',(req,res)=>{
         let {sessionId,serviceCode,phoneNumber,text} = req.body
             if(text == '' ){
@@ -40,5 +57,11 @@ function  processRequest() {
         console.log(`Server Listening on port ${port}`)
     })
 }
-
-processRequest();
+function getTime(){
+    let time = new Date()
+    console.log(time.getTimezoneOffset());
+    return time;
+}
+processBankTransaction();
+//processRequest();
+getTime();
